bef354fc3ad7bab8f60bc3a3399b055579be5477
Carson Wack <carson_wack@yahoo.com>
2019-03-06 11:18:38 -0800

    Updates

---
+++ precache-manifest.af96ea71208593baa5f05550c40195e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "443232e42a5d68b1c9a7",
+    "url": "/clicky-game/static/js/main.443232e4.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/clicky-game/static/js/2.a539ea9d.chunk.js"
+  },
+  {
+    "revision": "443232e42a5d68b1c9a7",
+    "url": "/clicky-game/static/css/main.efb3d5f5.chunk.css"
+  },
+  {
+    "revision": "8e2cfbfc361c1e259b38d5d5ae593198",
+    "url": "/clicky-game/index.html"
+  }
+];
