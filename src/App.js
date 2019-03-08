4d16d47816eeac14a7991d8f1abae58332771241
Carson Wack <carson_wack@yahoo.com>
2019-03-07 21:43:51 -0800

    publishing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import shuffle from "shuffle";
+import shuffle from "shuffle-array";
 
 let collection = [1,2,3,4,5];
 shuffle(collection);
 console.log(collection);
 
-const imgList = ['jinping', 'putin', 'trump', 'merkel', 'bezos', 'francis', 'gates', 'salman', 'modi', 'page', 'powell', 'macron'];
+
 
 
 class Game extends Component {
@@ -15,18 +15,20 @@ class Game extends Component {
     score: 0,
     topScore: 0,
     message: "Click an image to Begin!",
-    clicked: []
+    clicked: [],
+    imgList:  ['jinping', 'putin', 'trump', 'merkel', 'bezos', 'francis', 'gates', 'salman', 'modi', 'page', 'powell', 'macron']
   }
 
 
 
   handleClick = (name) => {
-    let { score, topScore, message, clicked } = this.state;
+    let { score, topScore, message, clicked, imgList } = this.state;
     if (clicked.includes(name)) {
       this.setState({
         score: 0,
         message: "Oops that was already clicked 😣. Try again!",
-        clicked: []
+        clicked: [],
+        imgList: shuffle(imgList)
       })
     } else {
       score += 1;
@@ -44,7 +46,8 @@ class Game extends Component {
         score: score,
         topScore: topScore,
         message: message,
-        clicked: clicked.concat([name])
+        clicked: clicked.concat([name]),
+        imgList: shuffle(imgList)
       })
     }
   };
@@ -71,10 +74,10 @@ class Game extends Component {
         <div className="container">
           <div col-md-8="true" offset-2="true">
             {
-              imgList.map(name =>
+              this.state.imgList.map(name =>
                 <img
                   key={name}
-                  src={require(`./public/img/${name}.png`)}
+                  src={require(`./../public/img/${name}.png`)}
                   alt={name}
                   className="namedImgs"
                   onClick={() => this.handleClick(name)}
